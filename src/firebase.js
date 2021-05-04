72ed10950cfc3b680fa296f3b756084f17f32a8e
Patrick Pirig <p.pirig@freenet.de>
2021-05-04 13:26:41 +0200

    db3

--- src/firebase.js
+++ src/firebase.js
@@ -10,3 +10,4 @@ const config = {
 };
 
 export const firebaseApp = firebase.initializeApp(config);
+export const firebaseDB = firebaseApp.database().ref().child("aufgabe");
--- src/pages/database.js
+++ src/pages/database.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
-function Database() {
-    return (
-        <div className='db'>
-            <h1>Database</h1>
-            <pre id="object">
+class Database extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            aufgabe: ""
+        }
+    }
 
-            </pre>
-        </div>
-    );
+    componentDidMount() {
+        console.log("comp did mount");
+    }
+
+    render() {
+        return (
+            <div className='db'>
+                <h1>Database</h1>
+
+            </div>
+        );
+    }
 }
 
 export default Database;
